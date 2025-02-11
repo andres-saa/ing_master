65c15329429dbf8754d92296fc17d8d8e4d5e647
andres-saa <andrew19f@gmail.com>
2025-02-10 23:36:57 -0500

    backend

--- frontend/src/service/conection.js
+++ frontend/src/service/conection.js
@@ -1,9 +1,9 @@
 // const URI = 'https://backend.novatocode.online'
-// const URI = 'https://tezos-back.arhook.com'
+const URI = 'https://backend.ing-master.com'
 // http://192.168.1.142:5173/
 // const URI = 'http://192.168.18.180:9800'
 
-const URI = 'http://localhost:8000'
+// const URI = 'http://localhost:8000'
 // const URI = 'https://backend.salchimonster.com'
 const URI_SOCKET = 'https://backend.salchimonster.com'
 export { URI, URI_SOCKET }
