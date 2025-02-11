fc8472fd54273df34257cf6aac8e8730f2006723
andres-saa <andrew19f@gmail.com>
2025-02-10 23:37:52 -0500

    final

--- frontend_delivery/src/service/conection.js
+++ frontend_delivery/src/service/conection.js
@@ -1,7 +1,7 @@
 // const URI = 'https://backend.novatocode.online'
 // const URI = 'https://tezos-back.arhook.com'
 // http://192.168.1.142:5173/
-const URI = 'http://localhost:8000'
+const URI = 'https://backend.ing-master.com'
 // const URI = 'http://167.71.178.54:8200'
 // const URI = 'http://back.novatocode.online'
 // const URI = 'https://backend.salchimonster.com'
